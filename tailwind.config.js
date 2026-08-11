8fd24872379cdc142adeba8625c73e49ca62a91f
musclesync <serefmina@gmail.com>
2026-08-11 16:27:06 +0200

    Downgrade Tailwind to v3 for Vercel compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        fadeIn: "fadeIn 1s ease forwards",
-        slideUp: "slideUp 1s ease forwards",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
