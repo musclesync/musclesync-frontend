5013eba9d7b29484e7e5bba09f475423d69478db
musclesync <serefmina@gmail.com>
2026-08-11 13:17:04 +0200

    Add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Features from "@/components/Features";
-import Pricing from "@/components/Pricing";
-import DashboardPreview from "@/components/DashboardPreview";
-import Footer from "@/components/Footer";
-
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <Navbar />
-      <Hero />
-      <Features />
-      <Pricing />
-      <DashboardPreview />
-      <Footer />
-    </main>
+    <h1>MuscleSync çalışıyor!</h1>
   );
-
 }
