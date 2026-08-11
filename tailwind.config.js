bf51276044ca8b07a82ab8c590b874dfff0ff5ff
musclesync <serefmina@gmail.com>
2026-08-11 14:06:38 +0200

    enable animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,19 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 1s ease forwards",
+        slideUp: "slideUp 1s ease forwards",
+      },
+    },
+  },
+  plugins: [],
+};
+
